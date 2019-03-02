12fa17d291d8bd4b2bc57807c01856159cf2c766
Marcello Cesar Alves de Souza Queiroz <Marcellocesar.queiroz@gmail.com>
2019-03-02 13:09:02 -0300

    #2 - Adicao problema para teste do algoritmo

--- index.php
+++ index.php
@@ -2,35 +2,67 @@
 
 require_once('simplexProblem.php');
 
-$problem = new SimplexProblem();
+//problem1();
+problem2();
 
-$restriction = new SimplexRestriction();
-$restriction->addVar(new SimplexVariable(1, 'x1'));
-$restriction->setRestrictionValue(4);
-$restriction->setRestrictionType(0);
-$problem->addRestriction($restriction);
+function problem2()
+{
+    $problem = new SimplexProblem();
 
-$restriction = new SimplexRestriction();
-$restriction->addVar(new SimplexVariable(1, 'x2'));
-$restriction->setRestrictionValue(6);
-$restriction->setRestrictionType(0);
-$problem->addRestriction($restriction);
+    $restriction = new SimplexRestriction();
+    $restriction->addVar(new SimplexVariable(2, 'Pedreiros'));
+    $restriction->addVar(new SimplexVariable(3, 'Serventes'));
+    $restriction->setRestrictionValue(30);
+    $restriction->setRestrictionType(0);
+    $problem->addRestriction($restriction);
 
-$restriction = new SimplexRestriction();
-$restriction->addVar(new SimplexVariable(3, 'x1'));
-$restriction->addVar(new SimplexVariable(2, 'x2'));
-$restriction->setRestrictionValue(18);
-$restriction->setRestrictionType(0);
-$problem->addRestriction($restriction);
+    $restriction = new SimplexRestriction();
+    $restriction->addVar(new SimplexVariable(4, 'Pedreiros'));
+    $restriction->addVar(new SimplexVariable(8, 'Serventes'));
+    $restriction->setRestrictionValue(70);
+    $restriction->setRestrictionType(0);
+    $problem->addRestriction($restriction);
 
-$objectiveFunction = new SimplexObjectiveFunction();
-$objectiveFunction->addVar(new SimplexVariable(3, 'x1'));
-$objectiveFunction->addVar(new SimplexVariable(5, 'x2'));
-$problem->setObjectiveFunction($objectiveFunction);
+    $objectiveFunction = new SimplexObjectiveFunction("LUCRO");
+    $objectiveFunction->addVar(new SimplexVariable(3000, 'Pedreiros'));
+    $objectiveFunction->addVar(new SimplexVariable(5000, 'Serventes'));
+    $problem->setObjectiveFunction($objectiveFunction);
 
-$problem->solve();
-//echo $problem->getSolutions();
-echo $problem->getBestSolution();
+    $problem->solve();
+    echo $problem->getBestSolution();
+}
+
+function problem1(){
+    $problem = new SimplexProblem();
+
+    $restriction = new SimplexRestriction();
+    $restriction->addVar(new SimplexVariable(1, 'x1'));
+    $restriction->setRestrictionValue(4);
+    $restriction->setRestrictionType(0);
+    $problem->addRestriction($restriction);
+
+    $restriction = new SimplexRestriction();
+    $restriction->addVar(new SimplexVariable(1, 'x2'));
+    $restriction->setRestrictionValue(6);
+    $restriction->setRestrictionType(0);
+    $problem->addRestriction($restriction);
+
+    $restriction = new SimplexRestriction();
+    $restriction->addVar(new SimplexVariable(3, 'x1'));
+    $restriction->addVar(new SimplexVariable(2, 'x2'));
+    $restriction->setRestrictionValue(18);
+    $restriction->setRestrictionType(0);
+    $problem->addRestriction($restriction);
+
+    $objectiveFunction = new SimplexObjectiveFunction();
+    $objectiveFunction->addVar(new SimplexVariable(3, 'x1'));
+    $objectiveFunction->addVar(new SimplexVariable(5, 'x2'));
+    $problem->setObjectiveFunction($objectiveFunction);
+
+    $problem->solve();
+    //echo $problem->getSolutions();
+    echo $problem->getBestSolution();
+}
 
 
 
